8133d075364b90588f0d7d1193972ed439d8a273
unknown <syaputraredho@gmail.com>
2022-09-12 15:29:16 +0700

    coba ngepush ke reposistory

---
+++ app/Http/Controllers/AngkaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AngkaController extends Controller
+{
+public function angka(){
+
+$angka = 12345;
+$angka = 12312312;
+$penjumlahan = $angka1 + $angka2;
+
+$huruf = "huruf";
+return $angka1;
+return $angka2;
+return $penjumlahan;
+}
+}
